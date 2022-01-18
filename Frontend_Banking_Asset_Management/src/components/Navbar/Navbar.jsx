eebea539e8c205016450b59e2789a35cfe285211
Andrejs Grocs <andrey.detochkin@gmail.com>
2022-01-18 13:20:42 +0100

    big update

--- Frontend_Banking_Asset_Management/src/components/Navbar/Navbar.jsx
+++ Frontend_Banking_Asset_Management/src/components/Navbar/Navbar.jsx
@@ -17,6 +17,9 @@ export default function Navbar() {
         <ul className='nav-list'>
         {loggedIn?( 
             <>
+            <li><Link to='/' exact activeClassName='active'> News</Link></li>
+        <li><Link to='/analytics' exact activeClassName='active'> Analytics</Link></li>
+        <li><Link to='/aboutus' exact activeClassName='active'>About US </Link></li>
             <li><Link to='/profile' exact activeClassName='active'> Profile</Link></li>
         <li><Link to='/logout' exact activeClassName='active'> Logout</Link></li>
           
