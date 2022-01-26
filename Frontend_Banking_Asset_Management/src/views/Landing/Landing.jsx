db369747a4ede75371e0c9b42d479b17e60052a8
Andrejs Grocs <andrey.detochkin@gmail.com>
2022-01-26 13:00:53 +0100

    carousel template finished

--- Frontend_Banking_Asset_Management/src/views/Landing/Landing.jsx
+++ Frontend_Banking_Asset_Management/src/views/Landing/Landing.jsx
@@ -23,10 +23,10 @@ export default function Landing() {
                    <img className='bannerImg' src={image3} />
                      <div className='bannerContent'>
                      <div>
-                       <h1 className="bannerHeader">Save the ocean environment</h1>
-                       <p className="bannerText">Investment in to the Eco ocean saving projects and more and more an more info in this sentence</p>
+                       <h1 className="bannerHeader1">Save the ocean environment</h1>
+                       <p className="bannerText1">Investment in to the Eco ocean saving projects and more and more an more info in this sentence</p>
                        </div>
-                        <button className='bannerButton'>Click here</button>
+                        <button className='bannerButton1'>Click here</button>
                     </div>
                     
          </div>
@@ -35,10 +35,10 @@ export default function Landing() {
                    <img className='bannerImg' src={image2} />
                      <div className='bannerContent'>
                      <div>
-                       <h1 className="bannerHeader">Save the ocean environment</h1>
-                       <p className="bannerText">Investment in to the Eco ocean saving projects and more and more an more info in this sentence</p>
+                       <h1 className="bannerHeader2">Save the ocean environment</h1>
+                       <p className="bannerText2">Investment in to the Eco ocean saving projects and more and more an more info in this sentence</p>
                        </div>
-                        <button className='bannerButton'>Click here</button>
+                        <button className='bannerButton2'>Click here</button>
                     </div>
                     
                 </div>
@@ -47,11 +47,29 @@ export default function Landing() {
                     <img className='bannerImg' src={image4} />
                      <div className='bannerContent'>
                      <div>
-                       <h1 className="bannerHeader">Save the ocean environment</h1>
-                       <p className="bannerText">Investment in to the Eco ocean saving projects and more and more an more info in this sentence</p>
+                       <h1 className="bannerHeader3">Save the ocean environment</h1>
+                       <p className="bannerText3">Investment in to the Eco ocean saving projects and more and more an more info in this sentence</p>
                        </div>
-                        <button className='bannerButton'>Click here</button>
+                        <button className='bannerButton3'>Click here</button>
                     </div>
+
+                    </div>
+                    <div className='banner'>
+                
+                   <iframe  height="800px" src="https://www.youtube.com/embed/IlFRPkT-hVc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                     <div className='bannerContent'>
+                     <div>
+                       <h1 className="bannerHeader3">Save the ocean environment</h1>
+                       <p className="bannerText3">Investment in to the Eco ocean saving projects and more and more an more info in this sentence</p>
+                       </div>
+                        <button className='bannerButton3'>Click here</button>
+                    </div>
+
+                    
+
+
+                      
+                    
                     
                 </div>
             </Carousel>
