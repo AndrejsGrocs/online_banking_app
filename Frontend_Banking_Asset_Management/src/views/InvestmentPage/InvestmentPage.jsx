0396354a7831323dc216698bdc04987c0360be60
Andrejs Grocs <andrey.detochkin@gmail.com>
2022-01-27 16:23:48 +0100

    money transfer connected to frontend

--- Frontend_Banking_Asset_Management/src/views/InvestmentPage/InvestmentPage.jsx
+++ Frontend_Banking_Asset_Management/src/views/InvestmentPage/InvestmentPage.jsx
@@ -4,7 +4,7 @@ function InvestmentPage() {
   return <div className='investment-page'>
             <h1>Investment Page</h1>
             <p>We are investing only in sustainable and eco projects.</p>
-
+             
         </div>
 }
 
