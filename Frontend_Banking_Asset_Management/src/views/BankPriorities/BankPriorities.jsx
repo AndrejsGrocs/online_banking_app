9e36b583a7f92b23cb60dc20529c5e8517615874
Andrejs Grocs <andrey.detochkin@gmail.com>
2022-02-08 16:10:43 +0100

    landing sign up designgs update

--- Frontend_Banking_Asset_Management/src/views/BankPriorities/BankPriorities.jsx
+++ Frontend_Banking_Asset_Management/src/views/BankPriorities/BankPriorities.jsx
@@ -4,16 +4,17 @@ import link from "../BankPrioritiesPage/BankPrioritiesPage"
 
 function BankPriorities() {
   return <div className='bank-priorities'>
+   <img className='section-image1'src={susecoimage} />
             <div className='bppflex'>
             <h1 className='bpph1'>Bank priorities Page</h1>
             <p className='bppp1'>Most banks use your money and invest it in guns, coal and other harmful industries - without your consent. We only invest your money sustainably.</p>
-            <button className='section-button' type="button" onClick={(e) => {
+            <button className='bannerButton1' type="button" onClick={(e) => {
       e.preventDefault();
       window.location.href='/mission';
-      }}>Click Me!</button>
+      }}>More Details</button>
             
             </div>
-            <img className='section-image'src={susecoimage} />
+           
             
         </div>
 }
