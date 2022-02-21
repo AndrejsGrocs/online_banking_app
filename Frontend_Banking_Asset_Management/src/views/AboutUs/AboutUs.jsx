fb9daf45a370e6beeef207ef4ad351a36df49ac5
Andrejs Grocs <andrey.detochkin@gmail.com>
2022-02-21 21:46:50 +0100

    about us under construction

--- Frontend_Banking_Asset_Management/src/views/AboutUs/AboutUs.jsx
+++ Frontend_Banking_Asset_Management/src/views/AboutUs/AboutUs.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import cardRobin from '../../assets/images/db-bank-AbouUs-Robin.png'
+import cardAndrejs from '../../assets/images/db-bank-AbouUs-Andrej.png'
 
 export default function AboutUs() {
     return (
-        <div className='sectionContainer'>
-            <h1>This is about us page</h1>
-            <h2>Here is the information about us</h2>
+        <div className='aboutUsContainer'>
+
+            <div className='cardContainer1'>
+                <div className='cardRobin'>
+                    <img className='image-robin' src={cardRobin} /> 
+                    <h2 className='about-us-card-header'>Robin Skladny</h2>
+                    <p className='about-us-card-text'>Backend/Frontend</p>
+                </div>
+
+
+
+                <div className='cardAndrejs'>
+
+                     <img className='image-andrejs' src={cardAndrejs} /> 
+                         <h2 className='about-us-card-header'>Andrejs Grocs</h2>
+                         <p className='about-us-card-text'>Frontend/Backend</p>
+                </div>
+
+
+
+
+            </div>
         </div>
     )
 }
